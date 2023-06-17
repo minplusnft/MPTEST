20982206af36a27c5edb4b8f1e9ecf4700cfb502
minplusnft <133950043+minplusnft@users.noreply.github.com>
2023-06-17 20:14:40 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const NFTCONTRACT = "0xF9aF453261fF863eE1C87226472fE6af560086e6";
+export const TOKENCONTRACT = "0x5745127dde6909a4A8E2AAB262cdac200807c8dd";
+export const STAKINGCONTRACT = "0xfD6cca10709a06d653bC0B93cC6f7c42Dea1579b";
+
+export const AIRDROPCONTRACT = "0x463D7224816Cfc827e5951e3382Fb1813731Bf00";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://bafybeibijqikgn6xynmw65ixpc5x5d6nx3zeoeawdie6prvyc2m4jrvokm.ipfs.w3s.link/";
